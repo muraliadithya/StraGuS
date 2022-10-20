91aa8aa383f4ba113aecd166e9aedf6a55020fac
muraliadithya <adithyamuralism@gmail.com>
2022-10-20 08:50:18 -0500

    Setting max-depth=1 in minisy. bug in tests.test_stragus.test_stragus_hub_randmodels

--- synth.py
+++ synth.py
@@ -116,7 +116,7 @@ def generate_grammar(signature: Sig, num_quantifiers: int, funcname):
 def synthesize_command(options):
     mode = options.get('mode', 'basic')
     if mode == 'basic':
-        command = str(minisy_path) + ' {}'
+        command = str(minisy_path) + ' --max-depth=1 {}'
     # Enumerative mode not supported for reading formulas from stdout in stream
     # elif mode == 'enum':
     #     command = 'cvc4 --lang=sygus2 --stream {}'
